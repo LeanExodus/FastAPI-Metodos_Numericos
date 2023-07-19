7710050291f42d5ceb95248beffc4c3cdc11d950
Leandro Campos <leandro-1018@hotmail.com>
2023-07-18 19:46:14 -0600

    CORS

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from routes.user import user
 from routes.secant import secant
 from routes.newton import newton
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(
     openapi_tags=[{
@@ -9,6 +12,16 @@ app = FastAPI(
         "name": "users",
         "description": "users routes"
     }]
+)
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
 )
 
 app.include_router(user)
